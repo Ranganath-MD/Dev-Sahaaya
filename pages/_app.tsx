19c3c4a80aff795764a9a18db66c28e209e55acd
Ranganath-MD <>
2021-10-05 22:22:11 +0530

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,44 @@
 import type { AppProps } from "next/app";
-import {
-  ChakraProvider,
-} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Layout } from "components/layout";
 import { Theme } from "styles/globalStyle";
 import { DefaultSeo } from "next-seo";
 import { seoConfig } from "components/seo/seo.config";
+import Script from "next/script";
+
 import "@fontsource/arvo";
 import "@fontsource/arvo/700.css";
 import "@fontsource/inter";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={Theme}>
-      
-      <Layout>
-        <DefaultSeo {...seoConfig} />
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <>
+      <Script
+        id="gtag"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ChakraProvider theme={Theme}>
+        <Layout>
+          <DefaultSeo {...seoConfig} />
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,18 +4,17 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
-
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" translate="no">
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/logo/logo.png" />
           <link
-            rel="icon"
+            rel="apple-touch-icon"
             href="/logo/logo.png"
           />
+          <link rel="icon" href="/logo/logo.png" />
         </Head>
         <body>
           <Main />
