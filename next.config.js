afd5665845afdda537f931c792adab7a37400a4b
Ranganath-MD <ranganath.developer@gmail.com>
2022-02-22 00:54:37 +0530

    changes in configs

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = withPWA({
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
     runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
   },
   eslint: {
     dirs: ["pages", "components", "utils", "types"],
